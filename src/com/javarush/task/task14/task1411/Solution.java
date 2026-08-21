3b0a00ec437cae5ebd34648fc6c710c285684569
devcontact-hub <317892548+devcontact-hub@users.noreply.github.com>
2026-08-21 19:25:55 +0300

    Java Core - Level 24 - Lecture 8 - Task 3

--- src/com/javarush/task/task14/task1411/Solution.java
+++ src/com/javarush/task/task14/task1411/Solution.java
@@ -16,17 +16,43 @@ public class Solution {
         //тут цикл по чтению ключей, пункт 1
         while (true) {
             key = reader.readLine();
-            boolean u =
-        }
-        {
+            boolean u = key.equals("user") || key.equals("loser") || key.equals("coder") || key.equals("proger");
+            if (!u)
+                break;
+
+            switch (key) {
+                case "user":
+                    person = new Person.User();
+                    break;
+                case "loser":
+                    person = new Person.Loser();
+                    break;
+                case "coder":
+                    person = new Person.Coder();
+                    break;
+                case "proger":
+                    person = new Person.Proger();
+                    break;
+            }
             //создаем объект, пункт 2
-
             doWork(person); //вызываем doWork
-
         }
     }
 
     public static void doWork(Person person) {
         // пункт 3
+        if (person instanceof Person.User) {
+            Person.User user = (Person.User) person;
+            user.live();
+        } else if (person instanceof Person.Loser) {
+            Person.Loser loser = (Person.Loser) person;
+            loser.doNothing();
+        } else if (person instanceof Person.Coder) {
+            Person.Coder coder = (Person.Coder) person;
+            coder.writeCode();
+        } else if (person instanceof Person.Proger) {
+            Person.Proger proger = (Person.Proger) person;
+            proger.enjoy();
+        }
     }
 }
