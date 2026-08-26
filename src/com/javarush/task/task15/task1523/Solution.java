eadccec3605c5fa0b50ce06c7c3fb633d5e17c2c
devcontact-hub <317892548+devcontact-hub@users.noreply.github.com>
2026-08-26 14:39:44 +0300

    Java Core - Level 25 - Lecture 12 - Task 8

---
+++ src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public Solution() {
+    }
+
+    protected Solution(String i) {
+    }
+
+    Solution(int i) {
+    }
+
+    private Solution(double i) {
+    }
+
+
+}
+
---
+++ src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution() {
+    }
+
+    protected SubSolution(String i) {
+        super(i);
+    }
+
+    SubSolution(int i) {
+        super(i);
+    }
+}
