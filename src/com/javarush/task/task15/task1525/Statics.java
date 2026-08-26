18ab03dd9280771a34f9d87be2411692adf4d8bc
devcontact-hub <317892548+devcontact-hub@users.noreply.github.com>
2026-08-26 23:45:14 +0300

    Java Core - Level 25 - Lecture 12 - Task 9

---
+++ src/com/javarush/task/task15/task1525/Statics.java
@@ -0,0 +1,5 @@
+package com.javarush.task.task15.task1525;
+
+public class Statics {
+    public static String FILE_NAME = "C:/Users/grigo/javarush/3203965/javarush-project/src/com/javarush/task/task15/task1525/sampleFile.txt";
+}
