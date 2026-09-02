b1b028bffe353e09df37c5568fd5b6f37218a4a0
devcontact-hub <317892548+devcontact-hub@users.noreply.github.com>
2026-09-02 18:49:03 +0300

    Java Core - Level 26 - Lecture 7 - Task 4

--- src/com/javarush/task/task16/task1614/Solution.java
+++ src/com/javarush/task/task16/task1614/Solution.java
@@ -39,6 +39,9 @@ public class Solution {
 
         public void printCountdown() throws InterruptedException {
             //add your code here - добавь код тут
+            countFrom--;
+            Thread.sleep(500);
+            System.out.println(list.get(countFrom));
         }
     }
 }
